a7936fce5284f70b62c158cd4dc175a9530fc2d3
idowu moses <dynamopraize5050@gmail.com>
2025-07-30 17:24:00 +0100

    first commit

---
+++ LongestCommonPrefixTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class LongestCommonPrefixTest {
+
+LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefixTest();
+
+
+@Test
+public void testLongestCommonPrefixWorks(){
+
+String [] input = {"
+
+  }
+
+}
