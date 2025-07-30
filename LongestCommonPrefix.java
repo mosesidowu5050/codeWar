a04ad83c6efe6e9e775d1ffb782d74c5d01c2793
idowu moses <dynamopraize5050@gmail.com>
2025-07-30 17:28:07 +0100

    first commit

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+public class LongestCommonPrefix {
+public static void main(String[] args){
+
+String [] input = {"flower", "flow", "flight"};
+String output = findLongestCommonPrefix(input);
+
+System.out.print(output);
+}
+
+
+
+}
